2ffbe33dff994f904a2c985394f98a9172904fe0
Kristina <kristina.tanya@gmail.com>
2019-12-26 11:08:32 -0800

    create basic user submission form

--- src/components/UserSubmissionForm.js
+++ src/components/UserSubmissionForm.js
@@ -5,14 +5,56 @@ class UserSubmissionForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      sentence: '',
     };
+
+    this.onFormChange = this.onFormChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormChange = (event) => {
+    let value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      sentence: '',
+    });
+
+    // invoke callback function from home
+    // that will make axios GET request to the back end
+    // then it will save the result within it's state
+    // and send the result to the Result comp.
   }
 
   render() {
     return (
       <div>
-        Form will appear here.
+        <h3>Submission Form</h3>
+        <form>
+          <div>
+            <input
+              placeholder="Enter your sentence here..."
+              type="text"
+              value={this.state.sentence}
+              name="sentence"
+              onChange={this.onFormChange}
+            />
+          </div>
+          <div>
+            <input 
+              type="submit"
+              value="Generate Result"
+              onClick={this.onFormSubmit}
+            />
+          </div>
+        </form>
       </div>
     )
   }
