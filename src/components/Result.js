e19950a86fb8ee409d071ad077c17dd485ff3bcc
Kristina <kristina.tanya@gmail.com>
2019-12-26 12:05:29 -0800

    tested out receiving results array of hardcoded data from backend API

--- src/components/Result.js
+++ src/components/Result.js
@@ -8,10 +8,38 @@ class Result extends React.Component {
     };
   }
 
+  // create a method that checks whether results is undefined
+  // if it's not undefined, iterate through the results array
+  // and display each element
+
+  displayResults(results) {
+    let resultsList;
+
+    if (results === undefined) {
+      return "undefined";
+    }
+    else {
+      resultsList = results.map((result, i) => {
+        console.log('KRISTINA');
+        console.log(result['sentence']['score']);
+        return result['sentence'];
+      });
+    }
+
+    return resultsList;
+  }
+
   render() {
+    console.log('result component is being rendered');
+    console.log(`${this.props.results}`);
+    let resultSentence = this.displayResults(this.props.results);
+    // resultSentence is an array of nested objects - should figure out a better way
+    // to iterate through these easily
+    console.log('result sentence');
+    console.log(resultSentence);
+    
     return (
       <div>
-        { this.props.results }
       </div>
     )
   }
