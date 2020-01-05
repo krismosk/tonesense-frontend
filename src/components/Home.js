acdf4562c2cad1c13eb5cfb2e9e94b657d245963
Kristina <kristina.tanya@gmail.com>
2020-01-04 19:12:08 -0800

    moved navbar to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import UserSubmissionForm from './UserSubmissionForm.js';
 import Result from './Result.js';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 class Home extends React.Component {
   constructor(props) {
@@ -44,21 +46,37 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <section>
-          <p>My app is built using the following technologies...</p>
-          <p>Interested in trying it out? Fill out the form below!</p>
-          <div>
-            <UserSubmissionForm 
-              getResult={this.getResult}
-              saveSubmission={this.saveSubmission}
-            />
-          </div>
-          <div>
-            <Result
-              results={this.state.results}
-            />
-          </div>
-        </section>
+        <nav>
+          <Navbar id="navbar-2" collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+            <Navbar.Brand href="#home">ToneSense</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+              </Nav>
+              <Nav>
+                <a className="nav-link" href="#features">Features</a>
+                <a className="nav-link" href="#demo">Demo</a>
+                <a className="nav-link" href="#contact">Contact</a>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </nav>
+        <div data-spy="scroll" data-target="#navbar-2" data-offset="0">
+            <h3 id="features">Features Section</h3>
+              <div>
+                <UserSubmissionForm 
+                  getResult={this.getResult}
+                  saveSubmission={this.saveSubmission}
+                />
+              </div>
+              <div>
+                <Result
+                  results={this.state.results}
+                />
+              </div>
+            <h3 id="demo">Demo Section</h3>
+            <h3 id="contact">Contact Section</h3>
+        </div>
       </div>
     )
   }
