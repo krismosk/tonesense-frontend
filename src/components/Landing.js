b2c5bcaceda649d53e8fbb26b12b69d607013646
Kristina <kristina.tanya@gmail.com>
2020-01-13 16:26:52 -0800

    redeploy with using different sizing for font

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ const Landing = (props) => {
 
   return (
   <div className="landing-container">
-      <h1 className="landing-title">Introducing ToneSense</h1>
+      <h1>Introducing ToneSense</h1>
       <h4>Effective writing powered by Machine Learning</h4>
       <small><p className="landing-slogan">Don't let a poorly worded email leave a bad taste — take control of your tone for successful writing.</p></small>
     </div>
